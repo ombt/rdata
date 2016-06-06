c1b47f9dcb7fbb3ffcbc95ff0c9c701036a49bc5
mike.a.rumore <mike.a.rumore@gmail.com>
2016-06-06 10:42:28 -0500

    skills update

--- skills/dataprep.R
+++ skills/dataprep.R
@@ -20,13 +20,13 @@ g.tp.cc <- grouped.tech.prod.comp.cnts
 g.tp.cc.cnt <- lapply(g.tp.cc, nrow)
 g.tp.cc.nz <- g.tp.cc[names(g.tp.cc.cnt[g.tp.cc.cnt!=0])]
 
-g.tp.cc.nz 
+# g.tp.cc.nz 
 
-for (tech in names(g.tp.cc.nz))
-{
-    print(paste("tech ...", tech))
-    print(g.tp.cc.nz[[tech]])
-}
+# for (tech in names(g.tp.cc.nz))
+# {
+    # print(paste("tech ...", tech))
+    # print(g.tp.cc.nz[[tech]])
+# }
 
 grouped.tech.prod.act.comp.cnts <- 
     split(comp.cnts, 
@@ -38,25 +38,22 @@ g.tpa.cc <- grouped.tech.prod.act.comp.cnts
 g.tpa.cc.cnt <- lapply(g.tpa.cc, nrow)
 g.tpa.cc.nz <- g.tpa.cc[names(g.tpa.cc.cnt[g.tpa.cc.cnt!=0])]
 
-g.tpa.cc.nz 
+# g.tpa.cc.nz 
 
-par(mfrow=c(3,2))
-
-pics = 0
+pcnt <- 0
 for (tech in names(g.tpa.cc.nz))
 {
-    pics = pics + 1
-    if ((pics%%6) == 1)
+    pcnt <- pcnt + 1
+    if ((pcnt%%6) == 1)
     {
         x11()
         par(mfrow=c(3,2))
-        pics = 0
     }
-    print(paste("tech ...", tech))
-    print(g.tpa.cc.nz[[tech]])
-    label = tech
-    labels = g.tpa.cc.nz[[tech]]$Level
-    slices = g.tpa.cc.nz[[tech]]$Cnt
+    # print(paste("tech ...", tech))
+    # print(g.tpa.cc.nz[[tech]])
+    label <- tech
+    labels <- g.tpa.cc.nz[[tech]]$Level
+    slices <- g.tpa.cc.nz[[tech]]$Cnt
     pie(slices, labels=labels, main=label)
 }
 
